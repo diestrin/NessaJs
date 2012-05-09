dafd1a6e406072110f4bf4c5591ab338805322d8
Oscar Viquez <eruecco87@gmail.com>
2012-05-08 22:59:28 -0600

    Added Fallbacks for database access logic

--- models/model.js
+++ models/model.js
@@ -1,12 +1,21 @@
 // Database Connection
-var mongo = require('mongodb-wrapper'),
-	db = mongo.db('localhost', 27017, 'test'),
+var mongo, db, collection,error;
+try{
+	mongo = require('mongodb-wrapper');
+	db = mongo.db('localhost', 27017, 'test');
     collection = db.collection('test');
+}catch(e){
+	error = e;
+}
 
 exports.getInfo = function(callback) {
-	
-	collection.find().toArray(function(err, result) {
-		callback(result[0].app);
-	});
-
+	if(!!collection)
+		collection.find().toArray(function(err, result) {
+			if(!err && !!result)
+				callback(null,result);
+			else
+				callback(err);
+		});
+	else
+		callback(error);
 };
